28f38eee12cf80981dd7b174eb861568774c7268
dimlu <dimlutine@gmail.com>
2022-05-19 18:10:26 -0400

    add user searching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import UserResults from "../components/users/UserResults"
 
 function Home() {
 
   return (
     <div>
-        <h1 className="text-6xl">Welcome</h1>
-        {process.env.REACT_APP_GITHUB_URL}
+      {/* search component will go here */}
+        <UserResults />
 
     </div>
   )
