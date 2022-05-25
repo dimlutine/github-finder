11c9eca0cbfc37bc569b023274141d079336de29
dimlu <dimlutine@gmail.com>
2022-05-24 20:08:48 -0400

    added search and clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,19 +14,30 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get intial users (testing)
-  const fetchUsers = async () => {
+  // Clear search results
+
+  const clearUsers = () => {
+    dispatch({ type: 'CLEAR_USERS' });
+  };
+
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
+    const { items } = await response.json();
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     });
   };
 
@@ -38,7 +49,8 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
       }}
     >
       {children}
