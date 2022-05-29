35819f7678c9adb837af593068dcc4fd08129a4b
dimlu <dimlutine@gmail.com>
2022-05-28 21:15:09 -0400

    refactor SearchUser to Actions file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -22,26 +22,6 @@ export const GithubProvider = ({ children }) => {
     dispatch({ type: 'CLEAR_USERS' });
   };
 
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const { items } = await response.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
-  };
   //Get a single user
   const getUser = async (login) => {
     setLoading();
@@ -95,11 +75,8 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getUserRepos,
